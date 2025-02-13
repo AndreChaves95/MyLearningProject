f0bb7062b83f8fef5b5c0599f2350429e41e116b
andre.chaves <andre.chaves@kuehne-nagel.com>
2025-02-13 16:52:58 +0000

    F7: Update Task and connect Repository, Service and Controller

--- project/src/main/java/com/andre/learning/app/TaskController.java
+++ project/src/main/java/com/andre/learning/app/TaskController.java
@@ -1,17 +1,37 @@
 package com.andre.learning.app;
 
+import com.andre.learning.domain.Task;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/tasks")
 public class TaskController {
 
-    public TaskController() {
+    // Class that handles the requests and responses
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
     }
 
-    @RequestMapping("/homepage")
+    @GetMapping("/homepage")
     public String home(){
         return "My Learning Project Starting Page!";
+    }
+
+    @ResponseStatus(HttpStatus.CREATED) // This will return status 201 with Created state
+    @PostMapping("/create")
+    public String createTask(@Valid @RequestBody Task task) {
+        taskService.createTask(task);
+        return "Task created successfully!";
     }
 }
--- project/src/main/java/com/andre/learning/app/TaskRepository.java
+++ project/src/main/java/com/andre/learning/app/TaskRepository.java
@@ -2,10 +2,12 @@ package com.andre.learning.app;
 
 import com.andre.learning.domain.Task;
 import org.springframework.jdbc.core.simple.JdbcClient;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-@Component // so it can be Spring to manage this
+@Repository // so it can be Spring to manage this
 public class TaskRepository {
+
+    // Class to manage the connection to the database
 
     private final JdbcClient jdbcClient;
 
@@ -14,10 +16,17 @@ public class TaskRepository {
     }
 
     public Task findById(Long id) {
-        String sql = "SELECT * FROM Task WHERE id = :id";
+        String sql = "SELECT * FROM TASK WHERE task_id = :id";
         return jdbcClient.sql(sql)
-                .param("id", id)
+                .param("task_id", id)
                 .query(Task.class)
                 .single();
+    }
+
+    public void createTask(Task task) {
+        String sql = "INSERT INTO TASK (task_id, title, description, completed, created_at, updated_at) VALUES (?,?,?,?,?,?)";
+        jdbcClient.sql(sql)
+                .params(task.getId(), task.getTitle(), task.getDescription(), task.isCompleted(), task.getCreatedAt(), task.getUpdatedAt())
+                .update();
     }
 }
