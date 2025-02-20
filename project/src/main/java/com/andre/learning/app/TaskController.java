0e9f7e6be2d382a1479f60a0b7cd150e41a1b75a
andre.chaves <andre.chaves@kuehne-nagel.com>
2025-02-20 16:49:45 +0000

    F11: Update Endpoints working

--- project/src/main/java/com/andre/learning/app/TaskController.java
+++ project/src/main/java/com/andre/learning/app/TaskController.java
@@ -1,9 +1,13 @@
 package com.andre.learning.app;
 
+import java.util.List;
+
+import com.andre.learning.customexceptions.TaskIdDuplicatedException;
 import com.andre.learning.domain.Task;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +15,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/tasks")
+@RequestMapping("/api/project")
 public class TaskController {
 
     // Class that handles the requests and responses
@@ -27,10 +31,21 @@ public class TaskController {
         return "My Learning Project Starting Page!";
     }
 
+    @GetMapping("/task/{id}")
+    public Task findById(@PathVariable Long id) {
+        return taskService.findById(id);
+    }
+
+    @GetMapping("/tasks")
+    public List<Task> findAll() {
+        return taskService.findAll();
+    }
+
     @ResponseStatus(HttpStatus.CREATED) // This will return status 201 with Created state
     @PostMapping("/create")
-    public String createTask(@Valid @RequestBody Task task) {
+    public String createTask(@Valid @RequestBody Task task) throws TaskIdDuplicatedException {
         taskService.createTask(task);
         return "Task created successfully!";
     }
+
 }
