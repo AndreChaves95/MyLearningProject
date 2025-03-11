349125000e64d88da7ceb0fff24715c02ab1a18a
andre.chaves <andre.chaves@kuehne-nagel.com>
2025-03-11 17:03:08 +0000

    F16: Add Exchange, Binding and JsonConverter to RabbitConfig

--- project/src/main/java/com/andre/learning/app/rabbitmq/RabbitConfig.java
+++ project/src/main/java/com/andre/learning/app/rabbitmq/RabbitConfig.java
@@ -1,8 +1,13 @@
 package com.andre.learning.app.rabbitmq;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.DefaultClassMapper;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,12 +21,35 @@ public class RabbitConfig {
         return new Queue(TASK_COMPLETE_QUEUE, false); //Create a new Queue with the name 'hello-rabbit'
     }
 
+    // The Exchange is responsible for routing the messages to the correct Queue.
     @Bean
+    public DirectExchange exchange() {
+        return new DirectExchange("rabbit-exchange");
+    }
+
+    // The Binding is responsible for bind the Queue to the Exchange.
+    @Bean
+    public Binding binding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with("routing-key");
+    }
+
     // The ConnectionFactory is responsible for creating connections to the RabbitMQ broker.
     // It encapsulates the configuration details like the broker's address, port, username, and password.
     // With this we can manage these details in one place and reuse the connection settings across the application.
+    @Bean
     public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
-        return new RabbitTemplate(connectionFactory);
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter jsonMessageConverter() {
+        Jackson2JsonMessageConverter converter = new Jackson2JsonMessageConverter();
+        DefaultClassMapper classMapper = new DefaultClassMapper();
+        classMapper.setTrustedPackages("com.andre.learning.domain");
+        converter.setClassMapper(classMapper);
+        return converter;
     }
 
 }
