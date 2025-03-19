3309f5b9a594564dd6478e97c2e444a5502b6a41
andre.chaves <andre.chaves@kuehne-nagel.com>
2025-03-19 16:12:05 +0000

    F20: Implement TestData mocks

---
+++ project/src/test/java/com/andre/learning/utils/TestData.java
@@ -0,0 +1,35 @@
+package com.andre.learning.utils;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+import com.andre.learning.domain.Task;
+import com.andre.learning.domain.TaskDTO;
+
+public final class TestData {
+
+    private TestData() {
+    }
+
+    public static TaskDTO buildTaskDto(int index) {
+        TaskDTO taskDTO = new TaskDTO();
+        taskDTO.setId((long) index);
+        taskDTO.setTitle("Task " + index);
+        taskDTO.setDescription("Task Description");
+        taskDTO.setCompleted(false);
+        taskDTO.setCreatedAt(LocalDateTime.now());
+        taskDTO.setUpdatedAt(LocalDateTime.now());
+        return taskDTO;
+    }
+
+    public static Task buildTask(int index) {
+        Task task = new Task();
+        task.setTaskId((long) index);
+        task.setTitle("Task " + index);
+        task.setDescription("Task Description");
+        task.setCompleted(false);
+        task.setCreatedAt(Timestamp.valueOf(LocalDateTime.now()));
+        task.setUpdatedAt(Timestamp.valueOf(LocalDateTime.now()));
+        return task;
+    }
+}
